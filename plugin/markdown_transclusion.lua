96a38ce153ebcbc732a2bd72ab6d16def873378e
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-04-10 22:00:18 +1000

    [bugfix] Issue with the command `:MarkdownTransclusionToggle`
    The issue was likely that the plugin was failing to load properly.

--- plugin/markdown_transclusion.lua
+++ plugin/markdown_transclusion.lua
@@ -1,2 +1,8 @@
 -- Entry point for plugin
-require('markdown_transclusion').setup()
+local setup_ok, markdown_transclusion = pcall(require, 'markdown_transclusion')
+if not setup_ok then
+  vim.notify('Failed to load markdown_transclusion plugin', vim.log.levels.ERROR)
+  return
+end
+
+markdown_transclusion.setup()
